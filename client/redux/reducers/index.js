41065d23f250d9952e4e993a63ad375afe71f0ce
HrdRgn <bobkovandv@gmail.com>
2020-06-23 22:54:17 +0300

    initial preparations

--- client/redux/reducers/index.js
+++ client/redux/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
+import users from './users'
+
 
 const createRootReducer = (history) =>
   combineReducers({
-    router: connectRouter(history)
+    router: connectRouter(history),
+    users
   })
 
 export default createRootReducer
---
+++ client/redux/reducers/users.js
@@ -0,0 +1,10 @@
+const initialState = {
+  testUser: 5
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
